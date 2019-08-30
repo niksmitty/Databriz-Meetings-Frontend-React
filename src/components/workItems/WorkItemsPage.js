72721cca9f6061d19d59c4eb2f1991f162570de9
niksmitty <niksmitty@rambler.ru>
2019-08-30 22:58:13 +0500

    Added makeRevision action by timer, also added fetchWorkItems action in case of revision is needed (first version).

--- src/components/workItems/WorkItemsPage.js
+++ src/components/workItems/WorkItemsPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import { connect } from 'react-redux';
 import { fetchWorkItems } from '../../services/workItems/actions';
+import { makeRevision, resetShouldUpdate } from "../../services/revision/actions";
 
 import PageHeader from "../pageHeader/PageHeader";
 import WorkItemRow from "../workItemRow/WorkItemRow";
@@ -9,23 +10,36 @@ import WorkItemRow from "../workItemRow/WorkItemRow";
 class WorkItemsPage extends Component {
 
     componentDidMount() {
-        const {fetchWorkItems} = this.props;
-        fetchWorkItems(
-            'egin@databriz.ru',
-            '11834e88-af42-4f6d-9d12-33acaa466655',
-            '17655e69-cfda-491c-9568-5c8a6a647078',
-            'Sibur\\Final'
-        );
+        const { makeRevision } = this.props;
+
+        this.timer = setInterval(() => {
+            const { revision } = this.props;
+            makeRevision(revision.revision)
+        }, 3000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+        this.timer = null;
     }
 
     render() {
-        const { workItems } = this.props;
+        const { workItemsInfo, revision, fetchWorkItems, resetShouldUpdate } = this.props;
 
-        const workItemRows = workItems.map(workItem => {
-            return (
-                <WorkItemRow key={workItem.id} title={workItem.title} />
-            )
-        });
+        if (revision.should_update) {
+            resetShouldUpdate();
+            fetchWorkItems();
+        }
+
+        let workItemRows = [];
+        if (workItemsInfo.hasOwnProperty("user_work_items")) {
+            let workItems = workItemsInfo["user_work_items"];
+            workItemRows = workItems.map(workItem => {
+                return (
+                    <WorkItemRow key={workItem.id} title={workItem.title} />
+                )
+            });
+        }
 
         return (
             <div>
@@ -38,11 +52,15 @@ class WorkItemsPage extends Component {
 }
 
 const mapStateToProps = state => ({
-    workItems: state.workItems.workItems
+    workItems: state.workItems.workItems,
+    workItemsInfo: state.workItems.workItemsInfo,
+    revision: state.revision.revision
 });
 
 const mapDispatchToProps = {
-    fetchWorkItems
+    fetchWorkItems,
+    makeRevision,
+    resetShouldUpdate
 };
 
 export default connect(
