9001d35c3dd7a27d768d7a1562dd60a08dd2eac7
niksmitty <niksmitty@rambler.ru>
2019-08-30 15:46:20 +0500

    Moved App component files to corresponding components directory, changed import paths.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 import { Provider as ReduxProvider } from 'react-redux';
-import configureStore from "./services/store";
+import configureStore from "../../services/store";
 
-// import ProjectsPage from "./components/projects/ProjectsPage";
-import WorkItemsPage from "./components/workItems/WorkItemsPage";
+// import ProjectsPage from "../projects/ProjectsPage";
+import WorkItemsPage from "../workItems/WorkItemsPage";
 
 const reduxStore = configureStore();
 
