6a393a4f5d93b743245e05dea059543b41bdb4eb
niksmitty <niksmitty@rambler.ru>
2019-09-03 21:49:45 +0500

    Removed unnecessary code and added watcherTasks file.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { Provider as ReduxProvider } from 'react-redux';
 import configureStore from "../../services/store";
 
-// import ProjectsPage from "../projects/ProjectsPage";
 import WorkItemsPage from "../workItems/WorkItemsPage";
 
 const reduxStore = configureStore();
@@ -13,7 +12,6 @@ function App() {
       <ReduxProvider store={reduxStore}>
           <div className="App">
               <div className="container-fluid mt-4">
-                  {/*<ProjectsPage />*/}
                   <WorkItemsPage />
               </div>
           </div>
